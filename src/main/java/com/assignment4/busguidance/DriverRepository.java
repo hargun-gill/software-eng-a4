6766cedd0d4b70857bed68d8e5aaa8a942767cf9
Hargun Gill <hargungillhargungill@gmail.com>
2026-06-01 12:30:49 +1000

    Driver class + driver repo code plan templates

--- src/main/java/com/assignment4/busguidance/DriverRepository.java
+++ src/main/java/com/assignment4/busguidance/DriverRepository.java
@@ -1,1 +1,11 @@
 package com.assignment4.busguidance;
+
+public boolean addDriver(Driver driver)
+public Driver retrieveDriver(String driverID)
+public boolean updateDriver(String driverID, Driver updatedDriver)
+public int countDrivers()
+
+public boolean isValidDriverID(String driverID)
+public boolean isValidAddress(String address)
+public boolean isValidBirthdate(String birthdate)
+public boolean isValidDriver(Driver driver)
